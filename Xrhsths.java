d61bdbee1f78d6ab485a7fb5a9120aa6fb7c2680
evmo13 <dimitris_evmo@hotmail.com>
2020-05-23 16:16:28 +0300

    Add files via upload

---
+++ Xrhsths.java
@@ -0,0 +1,28 @@
+
+public class Xrhsths {
+	
+	public String username;
+	public String password;
+	public String email;
+	public int  id;
+	public String announcement_feed; 
+	
+	Xrhsths(String username,String password,String email,int id,String announcement_feed){
+		
+	this.username=username;
+	this.password=password;
+	this.email=email;
+	this.id=id;
+	this.announcement_feed=announcement_feed;
+	
+	}
+	
+	public void viewFeed(String announcement_feed) {
+		System.out.print(this.announcement_feed);
+	
+	}
+	}
+		
+	
+
+
