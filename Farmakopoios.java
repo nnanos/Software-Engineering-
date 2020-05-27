1664e8fce9a9360714a59a49645a84aa4326fe18
ProFil96 <65950159+ProFil96@users.noreply.github.com>
2020-05-27 12:30:15 +0300

    Add files via upload

---
+++ Farmakopoios.java
@@ -0,0 +1,13 @@
+
+public class Farmakopoios extends Xrhsths 
+{
+	public String pharmacy;
+
+	public Farmakopoios(String username, String password, String email, int id, String announcement_feed, String pharmacy) 
+	{
+		super(username,password,email,id,announcement_feed);
+		this.pharmacy = pharmacy ;
+	}
+	
+	
+	}
