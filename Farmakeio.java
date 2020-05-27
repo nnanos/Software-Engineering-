2df5c325d031d5b6d33f8c71393b7ec1d945ef99
ProFil96 <65950159+ProFil96@users.noreply.github.com>
2020-05-27 16:08:37 +0300

    Add files via upload

---
+++ Farmakeio.java
@@ -0,0 +1,21 @@
+
+public class Farmakeio 
+{
+	public String name;
+	public String location;
+	public String schedule;
+	public String shift;
+	public String stock;
+	public String pharmacist;
+	
+	
+	public Farmakeio(String name, String location, String schedule, String shift, String stock, String pharmacist)
+	{
+		this.name = name;
+		this.location = location;
+		this.schedule = schedule;
+		this.shift= shift;
+		this.stock = stock;
+		this.pharmacist = pharmacist;
+	}
+}
