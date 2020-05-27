eb21a24ac625182267321decea3925bfff521c9b
nzervos98 <58821751+nzervos98@users.noreply.github.com>
2020-05-27 12:01:26 +0300

    Add files via upload

---
+++ ProgrammaTherapeias.java
@@ -0,0 +1,16 @@
+public class ProgrammaTherapeias {
+	private String med;
+	private String name;
+	private String date;
+	private String time;
+	private String notification;
+	
+	public ProgrammaTherapeias(String med, String name, String date, String time, String notification){
+		
+		this.med = med;
+		this.name = name;
+		this.date = date;
+		this.time = time;
+		this.notification = notification;
+	}
+}
