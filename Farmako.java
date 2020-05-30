51a0089968ab579c27eb5e4b0b91623c8546c345
nzervos98 <58821751+nzervos98@users.noreply.github.com>
2020-05-30 13:26:20 +0300

    Add files via upload

--- Farmako.java
+++ Farmako.java
@@ -1,17 +1,22 @@
 public class Farmako {
 
-	private String name;
+	private String[] name;
 	private int id;
 	private String description;
 	private int quantity;
 	private int price;
 	
-	public Farmako(String name, int id, String description, int quantity, int price) {
+	public Farmako(String[] name, int id, String description, int quantity, int price) {
 		
 		this.name = name;
 		this.id = id;
 		this.description = description;
 		this.quantity = quantity;
 		this.price = price;
+	}
+	
+	public void registerMed(Poliths civilian, Farmako med) {
+		
+		civilian.registered_meds = med.name;
 	}
 }
--- Poliths.java
+++ Poliths.java
@@ -1,10 +1,15 @@
 public class Poliths extends Xrhsths{
 	public String symptoms;
-	public String registered_meds;
+	public String[] registered_meds;
 	public int prescription_code;
+	public ProgrammaTherapeias therapy_program;
 	
-	public Poliths(String username, String password, String email, int id, String announcement_feed) {
+	public Poliths(String username, String password, String email, int id, String announcement_feed, ProgrammaTherapeias therapy_program) {
 		super(username, password, email, id, announcement_feed);
+		
+		this.registered_meds = registered_meds;
+		this.prescription_code = prescription_code;
+		this.therapy_program = therapy_program;
 	}
 	
 	//test main
