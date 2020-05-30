d616f0d17be1a93b07c65e15a5cf4d0ce6dc1b9c
nzervos98 <58821751+nzervos98@users.noreply.github.com>
2020-05-30 13:36:45 +0300

    Add files via upload

---
+++ Epikoinwnia.java
@@ -0,0 +1,18 @@
+public class Epikoinwnia {
+
+	public int date;
+	public int time;
+	public Xrhsths participants;
+	
+	public Epikoinwnia(int date, int time, Xrhsths participants) {
+		this.date = date;
+		this.time = time;
+		this.participants = participants;
+	}
+	
+	public void settleCommunication(participants.id, participants.id) {
+		
+		OthonhEpikoinwnias communicationScreen = new OthonhEpikoinwnias();
+	}
+
+}
