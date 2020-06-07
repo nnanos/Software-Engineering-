a34d5e40501e9873ce2478312a613f5dc8de1bf3
nnanos <46674903+nnanos@users.noreply.github.com>
2020-06-07 17:56:44 +0300

    Add files via upload

---
+++ iatrikos_foreas.java
@@ -0,0 +1,10 @@
+public class iatrikos_foreas{
+	protected String Category;
+	protected Analwsimos_Poros[] emergency_recources;
+	
+	public iatrikos_foreas(String Category, Analwsimos_Poros[] emergency_recources){
+		this.Category = Category;
+		this.emergency_recources = emergency_recources;
+	}
+	
+}	
