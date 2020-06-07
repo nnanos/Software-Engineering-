db3c8001d0c3da03ba8da4947928bfbe648cded4
nnanos <46674903+nnanos@users.noreply.github.com>
2020-06-07 18:22:27 +0300

    Update Astheneia.java

--- Astheneia.java
+++ Astheneia.java
@@ -15,4 +15,17 @@ public class Astheneia {
 		this.effective_meds = effective_meds;
 		this.num_of_cases_per_period = num_of_cases_per_period;
 	}
+	
+	public void deseasesPerPeriodGraph(int[] num_of_cases_per_period){
+    	}
+	
+	public int[] get_num_of_cases_per_period(int periods_selected){
+		int[] array = new int[num_of_cases_per_period.length];
+		for (int i = 0; i < periods_selected.length; i++) {
+			array[i] = num_of_cases_per_period[i];
+		
+		return array;
+	}
+		
+	
 }
