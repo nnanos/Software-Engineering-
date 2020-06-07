1cfb8a26ad56f13c840175291ed17414e5027af2
nnanos <46674903+nnanos@users.noreply.github.com>
2020-06-07 17:55:17 +0300

    Add files via upload

---
+++ Analwsimos_Poros.java
@@ -0,0 +1,18 @@
+public class Analwsimos_Poros{
+	protected String Kind;
+	protected int Quantity;
+	
+	public Analwsimos_Poros(String Kind, int Quantity){
+		this.Kind = Kind;
+		this.Quantity = Quantity;
+	}
+
+	public getQuantity(){
+		return Quantity;
+	}
+	
+	public getKind(){
+		return Kind;
+	}
+
+}
